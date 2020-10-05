767d6f05083f852bb0058d17a7772cf617832594
funroger@mail.ru <funroger@mail.ru>
2020-10-04 21:49:49 -0700

    [tools] NASM tools were added

---
+++ tools/win/build_asm.cmd
@@ -0,0 +1,41 @@
+@echo off
+
+SET SRC_FILE=%2
+SET DST_FILE=%3\%~n2.obj
+SET DIMANCHE=%~dp\..\..\..
+SET NASM=%DIMANCHE%\tools\win\nasm.exe
+
+SET DEBUG_FORMAT=
+SET PLATFORM=
+SET PREFIX=
+
+
+IF /I [%1]==[win32] (GOTO X86)
+IF /I [%1]==[x86] (GOTO X86)
+IF /I [%1]==[x64] (GOTO X86_64)
+
+
+:X86
+SET DEBUG_FORMAT=cv8
+SET PLATFORM=win32
+SET PREFIX="_"
+GOTO DO
+
+
+:X86_64
+SET DEBUG_FORMAT=cv8
+SET PLATFORM=win64
+SET PREFIX=""
+GOTO DO
+
+
+:DO
+%NASM% ^
+  -f %PLATFORM% ^
+  -g%DEBUG_FORMAT% ^
+  -I%DIMANCHE% ^
+  -o %DST_FILE% ^
+  --prefix %PREFIX% ^
+  -w+all ^
+  -Xvc ^
+  %SRC_FILE%
